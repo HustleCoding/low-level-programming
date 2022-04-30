553a56e19689f722cae16c61e1289ce8c3ad4511
HustleCoding <f.dobinciuc7@gmail.com>
2022-04-30 17:22:08 +0300

    First commit

---
+++ Proj1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello from Proj1" << endl;
+    return 0;
+}
---
+++ Proj2/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello from Proj2" << endl;
+    return 0;
+}
