2642c41cc5d017e2cc037f7a73b6723b4bb4de92
HustleCoding <f.dobinciuc7@gmail.com>
2022-05-01 23:44:12 +0300

    Add some integers to our project

---
+++ BasicIO/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    int num1;
+    int num2;
+    double num3;
+
+    cout << "Enter an integer: " << endl;
+    cin >> num1;
+    cout << "Your integer is " << num1 << endl;
+}
