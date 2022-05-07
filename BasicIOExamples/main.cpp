c7f6658e6d49e476eaeeab2feaefc4ce52826225
HustleCoding <f.dobinciuc7@gmail.com>
2022-05-07 16:41:31 +0300

    Add a file with exercises

---
+++ BasicIOExamples/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void say_hi()
+{
+    cout << "Hi Frank!";
+}
+
+void sallys_dogs()
+{
+    int x{3}
+
+        cout
+        << "Sally has " << x << " dogs.";
+}
+
+void date_of_birth()
+{
+    int m{};
+    int d{};
+    int y{};
+
+    cin >> m >> d >> y;
+
+    cout << m << " " << d << " " << y;
+}
