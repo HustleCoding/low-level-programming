c77b156fe4c35b3eedd5ed4afdf84e42b99955ee
HustleCoding <f.dobinciuc7@gmail.com>
2022-05-07 17:40:18 +0300

    Create a Varibles dir

---
+++ Variables/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+// This program will calculate the area of a room in square feet
+
+int main()
+{
+    int age{7};
+
+    cout << age << endl;
+
+    return 0;
+}
