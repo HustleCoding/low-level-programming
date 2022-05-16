5ee904d9b86297d259d2b81ee5dc2a625872ad1f
HustleCoding <f.dobinciuc7@gmail.com>
2022-05-16 22:41:39 +0300

    Add a new folder for practicing the fundamentals

---
+++ Fundamentals/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+    cout << "Your number is " << number << endl;
+}
