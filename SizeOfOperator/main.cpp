c98050dba7cc47b09fa0836100c2f951f36df884
HustleCoding <f.dobinciuc7@gmail.com>
2022-05-17 15:25:37 +0300

    Add a new folder with sizeof exercises

---
+++ SizeOfOperator/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "sizeof information" << endl;
+    cout << "===============================" << endl;
+
+    cout << "char: " << sizeof(char) << " bytes." << endl;
+    cout << "int: " << sizeof(int) << " bytes." << endl;
+}
