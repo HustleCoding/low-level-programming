45fdb0d3fa4dfc83775581e7c4162600229f7cde
HustleCoding <f.dobinciuc7@gmail.com>
2022-05-17 15:47:37 +0300

    Add more examples with sizeof

--- SizeOfOperator/main.cpp
+++ SizeOfOperator/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
@@ -9,4 +10,23 @@ int main()
 
     cout << "char: " << sizeof(char) << " bytes." << endl;
     cout << "int: " << sizeof(int) << " bytes." << endl;
+
+    // use values defined in <climits>
+    cout << "===============================" << endl;
+
+    cout << "Minimum values: " << endl;
+    cout << "char: " << CHAR_MIN << endl;
+    cout << "int: " << INT_MIN << endl;
+
+    // sizeof can also be used with variable names
+    cout << "===============================" << endl;
+    int age{21};
+    cout << "age is " << sizeof(age) << " bytes." << endl;
+    // or
+    cout << "age is " << sizeof age << " bytes." << endl;
+
+    double wage{22.34};
+    cout << "wage is " << sizeof(wage) << " bytes." << endl;
+    // or
+    cout << "wage is " << sizeof wage << " bytes." << endl;
 }
