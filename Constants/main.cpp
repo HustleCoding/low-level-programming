499d64fd62ca9aa7c260bfe93fafe64095fce60d
HustleCoding <f.dobinciuc7@gmail.com>
2022-05-17 17:38:48 +0300

    Refactor from hardcoded variables to constants

--- Constants/main.cpp
+++ Constants/main.cpp
@@ -10,14 +10,18 @@ int main()
     int number_of_rooms{0};
     cin >> number_of_rooms;
 
+    const double price_per_room{30};
+    const double sales_tax{0.06};
+    const int estimate_expiry{30}; // days
+
     cout << "\nEstimate for carpet cleaning service" << endl;
     cout << "Number of room: " << number_of_rooms << endl;
-    cout << "Price per room: $" << 30 << endl;
-    cout << "Cost: $" << 30 * number_of_rooms << endl;
-    cout << "Tax: $" << 30 * number_of_rooms * 0.06 << endl;
+    cout << "Price per room: $" << price_per_room << endl;
+    cout << "Cost: $" << price_per_room * number_of_rooms << endl;
+    cout << "Tax: $" << price_per_room * number_of_rooms * sales_tax << endl;
     cout << "==============================================" << endl;
-    cout << "Total estimate: $" << (30 * number_of_rooms) + (30 * number_of_rooms * 0.06) << endl;
-    cout << "This estimate is valid for " << 30 << " days" << endl;
+    cout << "Total estimate: $" << (price_per_room * number_of_rooms) + (price_per_room * number_of_rooms * sales_tax) << endl;
+    cout << "This estimate is valid for " << estimate_expiry << " days" << endl;
 
     cout << endl;
     return 0;
